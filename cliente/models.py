ed6b63b2dda0206b4701a9629239356b5e9b0758
BrunaBaia <baiabruna2708@gmail.com>
2026-02-20 21:55:17 -0300

    Add files via upload

---
+++ cliente/models.py
@@ -0,0 +1,9 @@
+from django.db import models
+
+# Create your models here.
+
+class Cliente:
+    def __init__(self, nome, telefone, email):
+        self.nome = nome
+        self.telefone = telefone
+        self.email = email
